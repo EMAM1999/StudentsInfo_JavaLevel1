06bf693f1173bcca7e59980bbf4fc589de5cd010
EMAM <65142314+EMAM1999@users.noreply.github.com>
2021-07-01 21:18:23 +0200

    Add files via upload

---
+++ myFirstProject/src/myfirstproject/DataTypesAndVariables.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstproject;
+
+/**
+
+ @author EMAM
+ */
+public class DataTypesAndVariables {
+
+      /**
+       @param args the command line arguments
+       */
+      public static void main(String[] args) {
+//                datatype variable = value ;
+//                datatype variable ;
+//                variable = value ;
+//                
+//          Numbers
+//                byte -128 : 127
+//                short -32768 : 32767
+//                int 
+//                long
+//                float
+//                double
+            byte x = 10;
+            int y = 20;
+            double z = x / y;
+            System.out.println(z);
+            System.out.println(x * y - z);
+//          Words
+//                char
+//                String 
+            char c = 'x';
+            System.out.println(c * 2);
+            c = 77;
+            System.out.println(c);
+//          Boolean
+//                boolean
+            boolean is;
+            is = true;
+            System.out.println(is);
+            is = false;
+            System.out.println(is);
+      }
+//ASCII code
+//Casting
+}
